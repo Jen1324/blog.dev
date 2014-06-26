963fd2766807a3cb72d2fff0a1e01d21fed4d802
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-06-26 11:37:50 -0500

    Initial Commit

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,35 @@
 |
 */
 
+// Route::get('sayhello/{name}', function($name)
+// {
+// 	return "Hello, $name";
+// }
+// Route::get('we are home.'));
+
+// Route::get('/sayhello/{name}', function($name)
+// {
+//     if ($name == "Jen")
+//     {
+//         return Redirect::to(['http://www.google.com.com']);
+//     }
+//     else
+//     {
+//         return "Hello, $name!";
+//     }
+// });
+
 Route::get('/', function()
 {
-	return View::make('hello');
+	return "This is home.";
+});
+
+Route::get('/resume', function()
+{
+	return "This is my resume.";
+});
+
+Route::get('/portfolio', function()
+{
+	return "This is my portfolio.";
 });
